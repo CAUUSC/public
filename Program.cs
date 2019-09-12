0283f62e8fe968f51518e63a2445b0160ae3d14c
CAUUSC <695725937@qq.com>
2019-09-13 04:09:02 +0800

    测试

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cshorp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a= 33;
+            Console.WriteLine(a);
+        }
+    }
+}
